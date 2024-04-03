45458d27607bd394dff8b8bc3a9222354e0c2280
MiguelAlvesMiguel <alvesmiguelm@gmail.com>
2024-04-03 04:03:58 +0100

    added health check

--- app.js
+++ app.js
@@ -21,6 +21,11 @@ app.get('/users', (req, res) => {
 app.get('/', (req, res) => {
     res.status(200).send(`Welcome to ReClaim API from server ${os.hostname()}!`);
 });
-
+// Health check endpoint
+app.get('/health', (req, res) => {
+    // Here you could add checks for your app's dependencies or other services
+    // For now, we're just sending back a 200 status code
+    res.status(200).json({ status: 'ok', uptime: process.uptime() });
+})
 const port = 3000;
 app.listen(port, () => console.log(`Server running on http://localhost:${port}`));
